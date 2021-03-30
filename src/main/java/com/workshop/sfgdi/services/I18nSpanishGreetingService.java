fcf4de94bd2548ec58a8729ce0cf627e778fef46
vma <mertaltas@gmail.com>
2021-03-30 19:14:11 +0300

    Lesson 39 workshop Day2

--- src/main/java/com/workshop/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/com/workshop/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package com.workshop.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES","default"})
 @Service("I18nService")
 public class I18nSpanishGreetingService implements GreetingService{
 
